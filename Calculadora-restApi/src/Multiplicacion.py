94c2c421ae8d29a00801f174e323955dc2310b59
JulioCesarReina94 <61439899+JulioCesarReina94@users.noreply.github.com>
2020-04-19 12:14:57 -0500

    Actualizacion microservicios

---
+++ Calculadora-restApi/src/Multiplicacion.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+respuesta = {"code": 200, "desc": "OK"}
+
+@app.route('/multiplicar', methods=['POST'])
+def  multiplicar():
+	new_operacion = {
+		"numeroUno": request.json['numeroUno'],
+		"numeroDos": request.json['numeroDos'],
+		"resultado": request.json['numeroUno'] * request.json['numeroDos']
+	}
+	return jsonify({"Status": respuesta,"Resultado": new_operacion})
+
+
+if __name__ == '__main__':
+	app.run(host= "0.0.0.0",debug=True, port = 100)
